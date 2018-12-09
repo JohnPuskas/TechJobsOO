6ae404072645c0e22d2f02ea1d36f2d5b0767a5f
John Puskas <john.h.puskas@gmail.com>
2018-12-09 14:37:24 -0600

    Refactoring with BaseViewModel works.

---
+++ src/TechJobs/ViewModels/BaseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechJobs.Models;
+
+namespace TechJobs.ViewModels
+{
+    public class BaseViewModel
+    {
+        public string Title { get; set; } = "";
+
+        public List<JobFieldType> Columns { get; set; }
+
+        public JobFieldType Column { get; set; }   //<---How to address assignment of value to All in SJVM?
+
+
+        public void PopulateColumnList()   // <--Do I need a parameter here?
+        {
+            Columns = new List<JobFieldType>();
+
+            foreach (JobFieldType enumVal in Enum.GetValues(typeof(JobFieldType)))
+            {
+                Columns.Add(enumVal);
+            }
+        }
+    }
+}
